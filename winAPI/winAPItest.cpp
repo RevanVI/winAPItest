79484e3f0b63edca0ba4aaa3e9a8264badca3f80
RevanVI <revan0603@gmail.com>
2019-03-26 17:09:45 +0300

    start lab3

--- winAPI/winAPItest.cpp
+++ winAPI/winAPItest.cpp
@@ -144,12 +144,12 @@ LRESULT CALLBACK MainWndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lPar
 	int minC = 10, maxC = 60, defC = 40;
 	double minPos = 0.25, maxPos = 0.75, defPos = 0.5;
 
-	static int m = 11;
-	static int n = 4;
+	static int m = 4;
+	static int n = 16;
 	static matrix p0(m, n); //изначальные точки
-	static matrix* p;
+	static matrix** p;
 	static int num = 0;
-	static matrix* J;
+	static matrix J(4, 4);
 	static matrix axis(2, 4); //ось поворота
 	static double angle; //угол поворота
 	static int pNum;
@@ -172,25 +172,42 @@ LRESULT CALLBACK MainWndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lPar
 		d.cx = 40;
 		d.cz = 40;
 
-		p0(0, 0) = 0;		p0(0, 1) = 0;		p0(0, 2) = 0;		p0(0, 3) = 1;
-		p0(1, 0) = 1;		p0(1, 1) = 2;		p0(1, 2) = 0;		p0(1, 3) = 1;
-		p0(2, 0) = 2;		p0(2, 1) = 0;		p0(2, 2) = 0;		p0(2, 3) = 1;
-		p0(3, 0) = 3;		p0(3, 1) = -2;		p0(3, 2) = 0;		p0(3, 3) = 1;
-		p0(4, 0) = 4;		p0(4, 1) = 0;		p0(4, 2) = 0;		p0(4, 3) = 1;
-		p0(5, 0) = 5;		p0(5, 1) = 2;		p0(5, 2) = 0;		p0(5, 3) = 1;
-		p0(6, 0) = 6;		p0(6, 1) = 0;		p0(6, 2) = 0;		p0(6, 3) = 1;
-		p0(7, 0) = 7;		p0(7, 1) = -2;		p0(7, 2) = 0;		p0(7, 3) = 1;
-		p0(8, 0) = 8;		p0(8, 1) = 0;		p0(8, 2) = 0;		p0(8, 3) = 1;
-		p0(9, 0) = 9;		p0(9, 1) = 2;		p0(9, 2) = 0;		p0(9, 3) = 1;
-		p0(10, 0) = 10;		p0(10, 1) = 0;		p0(10, 2) = 0;		p0(10, 3) = 1;
 
-		p = calculatePoints(p0, num);
+		p0(0, 0) = -3;		p0(0, 1) = 0;		p0(0, 2) = 3;		p0(0, 3) = 1;
+		p0(1, 0) = -1;		p0(1, 1) = 1;		p0(1, 2) = 3;		p0(1, 3) = 1;
+		p0(2, 0) = 1;		p0(2, 1) = 1;		p0(2, 2) = 3;		p0(2, 3) = 1;
+		p0(3, 0) = 3;		p0(3, 1) = 0;		p0(3, 2) = 3;		p0(3, 3) = 1;
+
+		p0(0, 4) = -3;		p0(0, 5) = 1;		p0(0, 6) = 1;		p0(0, 7) = 1;
+		p0(1, 4) = -1;		p0(1, 5) = 1;		p0(1, 6) = 1;		p0(1, 7) = 1;
+		p0(2, 4) = 1;		p0(2, 5) = 1;		p0(2, 6) = 1;		p0(2, 7) = 1;
+		p0(3, 4) = 3;		p0(3, 5) = 1;		p0(3, 6) = 1;		p0(3, 7) = 1;
+
+		p0(0, 8) = -3;		p0(0, 9) = 1;		p0(0, 10) = -1;		p0(0, 11) = 1;
+		p0(1, 8) = -1;		p0(1, 9) = 1;		p0(1, 10) = -1;		p0(1, 11) = 1;
+		p0(2, 8) = 1;		p0(2, 9) = 1;		p0(2, 10) = -1;		p0(2, 11) = 1;
+		p0(3, 8) = 3;		p0(3, 9) = 1;		p0(3, 10) = -1;		p0(3, 11) = 1;
+
+		p0(0, 12) = -3;		p0(0, 13) = 0;		p0(0, 14) = -3;		p0(0, 15) = 1;
+		p0(1, 12) = -1;		p0(1, 13) = 1;		p0(1, 14) = -3;		p0(1, 15) = 1;
+		p0(2, 12) = 1;		p0(2, 13) = 1;		p0(2, 14) = -3;		p0(2, 15) = 1;
+		p0(3, 12) = 3;		p0(3, 13) = 0;		p0(3, 14) = -3;		p0(3, 15) = 1;
+
+		p = new matrix*[4];
+		for (int i = 0; i < 4; ++i)
+			p[i] = new matrix[4];
+
 		double dt = 1 / (m * 4.0);
 		int jC = int(1 / dt);
 		double t = 0;
-		J = new matrix(jC, 4);
-		J->fill(0);
-		for (int i = 0; i < jC; ++i)
+		//J = new matrix(4, 4);
+		//J->fill(0);
+		J(0, 0) = -1;		J(0, 1) = 3;		J(0, 2) = -3;		J(0, 3) = 1;
+		J(1, 0) = 3;		J(1, 1) = -6;		J(1, 2) = 3;		J(1, 3) = 0;
+		J(2, 0) = -3;		J(2, 1) = 3;		J(2, 2) = 0;		J(2, 3) = 0;
+		J(3, 0) = 1;		J(3, 1) = 0;		J(3, 2) = 0;		J(3, 3) = 0;
+		/*
+		for (int i = 0; i < 4; ++i)
 		{
 			(*J)(i, 0) = pow((1 - t), 3);
 			(*J)(i, 1) = 3 * t * pow((1 - t), 2);
@@ -200,7 +217,7 @@ LRESULT CALLBACK MainWndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lPar
 			if (i + 1 == jC - 1)
 				t = 1;
 		}
-
+		*/
 		axis(0, 0) = 0; axis(0, 1) = 0; axis(0, 2) = 0; axis(0, 3) = 1;
 		axis(1, 0) = 0; axis(1, 1) = 10; axis(1, 2) = 0; axis(1, 3) = 1;
 		angle = 0 * PI / 180;
@@ -317,6 +334,7 @@ LRESULT CALLBACK MainWndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lPar
 			x + 0.1 * rcWnd.right, y, 0.1 * rcWnd.right, 0.04 * rcWnd.bottom, hWnd, (HMENU)ID_BUT1, hInst, NULL);
 	}
 	break;
+	/*
 	case WM_COMMAND:
 	{
 		TCHAR buf[10];
@@ -636,6 +654,7 @@ LRESULT CALLBACK MainWndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lPar
 		}
 	}
 	break;
+	*/
 	case WM_PAINT:
 		{
 		PAINTSTRUCT ps;
@@ -665,21 +684,66 @@ LRESULT CALLBACK MainWndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lPar
 		}
 
 		create3DGrid(hdc, d, mode);
-		if (!viewChange && !pointChange)
-		{
-			for (int i = 0; i < num; ++i)
-				p[i] = rotateFig(p[i], axis, angle);
+			//for (int i = 0; i < num; ++i)
+				//p[i] = rotateFig(p[i], axis, angle);
+			double du = 1 / (m * 4.0);
+			int uC = int(1 / du);
+			double u = 0.0;
+
+			double dw = 1 / (m * 4.0);
+			int wC = int(1 / dw);
+			double w = 0.0;
 			p0 = rotateFig(p0, axis, angle);
-		}
+
+			matrix oldDraw(wC, 4);
+			matrix draw(wC, 4);
+
+			matrix vU(1, 4);
+			matrix vW(4, 1);
+			for (int i = 0; i < uC; ++i)
+			{
+				//if (i != 0)
+				//	oldDraw = draw;
+				vU(0, 0) = pow(u, 3);
+				vU(0, 1) = pow(u, 2);
+				vU(0, 2) = pow(u, 1);
+				vU(0, 3) = 1;
+				for (int j = 0; j < wC; ++j)
+				{
+					vW(0, 1) = pow(w, 3);
+					vW(1, 1) = pow(w, 2);
+					vW(2, 1) = pow(w, 1);
+					vW(3, 1) = 1;
+					for (int coord = 0; coord < 3; ++coord)
+					{
+						matrix subMP(4, 4);
+						for (int k = 0; k < 4; ++k)
+							for (int l = 0; l < 4; ++l)
+								subMP(k, l) = p0(k, l * 4 + coord);
+						draw(j, coord) = (vU * J * subMP * J * vW)(0, 0);
+					}
+					draw(j, 3) = 1;
+					w += dw;
+					if (j + 1 == wC - 1)
+						w = 1;
+				}
+				draw = draw * dim;
+				drawPol3Dim(hdc, draw, RGB(0, 0, 0), d);
+				u += du;
+				if (i + 1 == uC - 1)
+					u = 1;
+			}
 		
+		/*
 		for (int i = 0; i < num; ++i)
 		{
-			matrix draw = ((*J) * p[i]);
+			matrix draw = J * p[i];
 			draw = draw * dim;
 			drawPol3Dim(hdc, draw, RGB(0, 0, 0), d);
 		}
 		matrix draw = p0 * dim;
 		drawPol3Dim(hdc, draw, RGB(120, 120, 120), d);
+		*/
 		EndPaint(hWnd, &ps);
 		viewChange = false;
 
@@ -710,8 +774,8 @@ LRESULT CALLBACK MainWndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lPar
 	}
 		break;
     case WM_DESTROY:
-		delete J;
-		delete[] p;
+		//delete J;
+		//delete[] p;
         PostQuitMessage(0);
         break;
     default:
